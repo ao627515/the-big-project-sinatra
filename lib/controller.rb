08a8f8ad2967cdcf03017ba51b1c3929208eca73
ao627515 <ao627515@gmail.com>
2024-08-06 07:54:45 +0000

    A new gossip !

--- lib/controller.rb
+++ lib/controller.rb
@@ -2,7 +2,10 @@ class ApplicationController < Sinatra::Base
   get '/' do
     erb :index
   end
-  get 'gossips/new' do
-    erb :index
+  get '/gossips/new/' do
+    erb :new_gossip
+  end
+  post '/gossips/new/' do
+    puts 'Ce programme ne fait rien pour le moment, on va donc afficher un message dans le terminal'
   end
 end
